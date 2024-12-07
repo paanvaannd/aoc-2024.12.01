65873cbfc63e0b40682ce542781d8e4f043ed68f
Pavan Anand <pavan.anand@pm.me>
2024-12-06 20:19:47 -0800

    feat!: sum distance differences
    Lines from `data/distances.txt` are ingested into separate lists & sorted in ascending order. The differences between each pair of values in both lists is then calculated for all pairs of data. These differences are then summed and reported in a formatted string.
    
    This correctly answers Puzzle 1 of the December 1st, 2024 Advent of Code.

--- minimize_distance/minimize_distance.py
+++ minimize_distance/minimize_distance.py
@@ -7,6 +7,21 @@ script_path = os.path.abspath(__file__)
 script_root = os.path.dirname(script_path)
 project_root = os.path.dirname(script_root)
 
-data_file = os.path.join(project_root, "data", "")
+data_file = os.path.join(project_root, "data", "distances.txt")
+
+left_column:list = []
+right_column:list = []
 with open(data_file) as data:
-    pass
+    for line in data:
+        left_datum, right_datum = line.strip().split()
+        left_column.append(int(left_datum))
+        right_column.append(int(right_datum))
+
+left_column.sort()
+right_column.sort()
+total_difference: int = sum(abs(left_datum - right_datum) for left_datum, right_datum in zip(left_column, right_column))
+
+print("\n",
+      "Part 1\n",
+      "------\n",
+      f"The total difference of distances between lists is equal to {total_difference:,} units.")
